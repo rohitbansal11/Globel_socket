eb34cbadfcd9db18097648f9251582b3e9722274
rohitbansal11 <80095887+rohitbansal11@users.noreply.github.com>
2022-08-26 10:05:17 +0530

    Update multer.js

--- multer.js
+++ multer.js
@@ -1,14 +1,62 @@
-const multer = require("multer");
-module.exports = () => {
-  const storage = multer.diskStorage({
-    destination(req, file, callback) {
-      callback(null, "./uploads");
-    },
-    filename(req, file, callback) {
-      const fileName = file.originalname.toLowerCase().split(" ").join("-");
-      req.body.fileName = fileName;
-      callback(null, Date.now() + "-" + fileName);
-    },
-  });
-  return multer({ storage });
+const DIR = "./uploads/ck";
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, DIR);
+    console.log(DIR, "in the dir");
+  },
+  filename: (req, file, cb) => {
+    const date = Date.now();
+    const fileName = file.originalname.toLowerCase().split(" ").join("-");
+    req.body.fileName = date + "-" + fileName;
+    cb(null, date + "-" + fileName);
+  },
+});
+
+var upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    cb(null, true);
+  },
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+router.post(
+  "/ckImageUpload",
+  authentication,
+  upload.single("ckImage"),
+  emailController.getCkImage
+);
+
+
+const getCkImage = async (req, res) => {
+  try {
+    return res.status(200).send({
+      status: 200,
+      url: `${process.env.API_BASE_URL}/uploads/ck/${req.body.fileName}`,
+      message: "image uploaded  successfully",
+    });
+  } catch (err) {
+    return res.status(500).send({
+      status: 500,
+      message: "Something went wrong, please try again later!",
+      error: err.message,
+    });
+  }
 };
+
+
+
+
+
+
